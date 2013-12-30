2fddc09b0fc8a23054e168b504810e3cc3acd294
Asher Krim <asher.krim@nyu.edu>
2013-12-30 12:33:41 +0200

    first commit

---
+++ ExternalDATA.js
@@ -0,0 +1,19 @@
+function getRemoteData() {
+	
+var sendData = 
+[{"ID":"100", "type":"Man-made object", "Name":"Mars 2", "Nationality":"USSR", "Landed":"1971", "Mass":"1210", "Status":"Failure during descent; crashed on surface", "Location":"", "LongLetter":"S", "Long":"45", "LatLetter":"W", "Lat":"313"}
+,{"ID":"101", "type":"Man-made object", "Name":"Mars 3", "Nationality":"USSR", "Landed":"1971", "Mass":"1210", "Status":"Transmission failure 14.5 seconds after soft landing", "Location":"Sirenum Terra", "LongLetter":"S", "Long":"45", "LatLetter":"W", "Lat":"158"}
+,{"ID":"102", "type":"Man-made object", "Name":"Mars 6", "Nationality":"USSR", "Landed":"1973", "Mass":"635", "Status":"Insufficient thrust during landing; crashed on surface", "Location":"Margaritifer Terra", "LongLetter":"S", "Long":"23.9", "LatLetter":"W", "Lat":"19.42"}
+,{"ID":"103", "type":"Man-made object", "Name":"Viking 1 lander", "Nationality":"USA", "Landed":"1976", "Mass":"657", "Status":"Operated 2245 sols (martian days). Last contact Nov 11, 1982", "Location":"Chryse Planitia", "LongLetter":"N", "Long":"22.697", "LatLetter":"W", "Lat":"48.222"}
+,{"ID":"104", "type":"Man-made object", "Name":"Viking 2 lander", "Nationality":"USA", "Landed":"1976", "Mass":"657", "Status":"Operated 1281 sols. Last contact Apr 11, 1980", "Location":"Utopia Planitia", "LongLetter":"N", "Long":"48.269", "LatLetter":"W", "Lat":"225.990"}
+,{"ID":"105", "type":"Man-made object", "Name":"Mars Pathfinder & Sojourner rover", "Nationality":"USA", "Landed":"1997", "Mass":"360", "Status":"Operated 83 sols. Last contact Sep 27, 1997", "Location":"Areas Vallis", "LongLetter":"N", "Long":"19.33", "LatLetter":"W", "Lat":"33.55"}
+,{"ID":"106", "type":"Man-made object", "Name":"Mars Polar Lander & Deep Space 2", "Nationality":"USA", "Landed":"1999", "Mass":"500", "Status":"Unknown failure during descent; crashed on surface", "Location":"Ultimi Scopuli", "LongLetter":"S", "Long":"76", "LatLetter":"W", "Lat":"195"}
+,{"ID":"107", "type":"Man-made object", "Name":"Spirit Rover", "Nationality":"USA", "Landed":"2004", "Mass":"185", "Status":"Operated 2210 sols. Last contact Mar 22, 2010", "Location":"Gustev crater", "LongLetter":"S", "Long":"14.5718", "LatLetter":"E", "Lat":"175.4785"}
+,{"ID":"108", "type":"Man-made object", "Name":"Opportunity Rover", "Nationality":"USA", "Landed":"2004", "Mass":"185", "Status":"Still in operation", "Location":"Meridiani Planum", "LongLetter":"S", "Long":"1.9462", "LatLetter":"E", "Lat":"354.4734"}
+,{"ID":"109", "type":"Man-made object", "Name":"Pheonix Mars Lander", "Nationality":"USA", "Landed":"2008", "Mass":"350", "Status":"Operated 155 sols. Last contact Nov 2, 2008", "Location":"Vastitas Borealis (Green Valley)", "LongLetter":"N", "Long":"68.15", "LatLetter":"W", "Lat":"125.9"}
+,{"ID":"110", "type":"Man-made object", "Name":"Mars Science Laboratory (Curiosity)", "Nationality":"USA", "Landed":"2012", "Mass":"900", "Status":"Still in operation", "Location":"Aeolis Palus in Gale Crater", "LongLetter":"S", "Long":"5.4", "LatLetter":"E", "Lat":"137.8"}
+];
+
+return sendData;
+
+}
